61fa7d0c2d11320db8705fb0900b1ddd77c758b2
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-07-31 18:40:14 +0530

    Create Pattern1.py

---
+++ Pattern1.py
@@ -0,0 +1,18 @@
+
+
+def printstar(n):
+    for i in range(n):
+        for j in range(i+1):
+            print("*",end=" ")
+
+        print()
+
+if __name__== "__main__":
+
+    try:
+                   
+         n = int(input("ENter the number"))
+         printstar(n)
+    except ValueError:
+      print("sjcbkjshkjh")
+        
