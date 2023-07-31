3511120a3bd6b85fc22974510b9a94d83b1ff785
Daksh <115346924+Daksh7112003@users.noreply.github.com>
2023-07-31 16:41:26 +0530

    Create isleapyear.py

---
+++ isleapyear.py
@@ -0,0 +1,16 @@
+    
+def is_leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    try:
+        year = int(input("Enter a year: "))
+        if is_leap_year(year):
+            print(f"{year} is a leap year.")
+        else:
+            print(f"{year} is not a leap year.")
+    except ValueError:
+        print("Invalid input! Please enter a valid integer.")
