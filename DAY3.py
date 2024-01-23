13794a81e3f3e37712472a8c4c3e8870f56dd7f5
Daksh7112003 <13anil67@gmail.com>
2024-01-23 19:21:36 +0530

    DAY3

---
+++ DAY3.py
@@ -0,0 +1,21 @@
+#set 
+
+s1={1, "a", True, "b", False}
+s1.add("Hello")
+# print(s1)
+
+#if we want to add multiple element then we just need to update the dictionary.......
+
+
+set1={1,2,3}
+set2={"a","b","d" , 3,2}
+# set1=set1.union(set2)
+
+print(set1.union(set2))
+
+
+
+#intersection
+
+
+print(set1.intersection(set2))
