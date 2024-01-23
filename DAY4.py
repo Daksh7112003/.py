e716cc7a94831adc263ea877f0d98184913f7378
Daksh7112003 <13anil67@gmail.com>
2024-01-23 19:22:08 +0530

    DAY4

---
+++ DAY4.py
@@ -0,0 +1,40 @@
+#statements...
+
+
+
+a=10
+b=20
+if(a>b):
+    print("a is greater than b ")
+else:
+    print("bla bla")
+
+#if with tuples
+
+tup={a,1,'p','l',5,12}
+# if 12 in tup:
+#     print("japan")
+
+if 1909 in tup:
+    print("japan")
+else:
+    print("hsdghfkhsakg")
+
+
+
+#if with list....
+
+
+
+list=[1,2,3,4,5,1]
+    
+if 3 in list:
+    print("jahdad")
+    
+if list[2]==3 in list:
+    print('sjkhdh')
+    if list[0]==list[5] in list:
+        print("djhfkhsk")
+
+    
+
