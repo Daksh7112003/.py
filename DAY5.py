7222cdd322c1c987759b031d4eb3ef9626cedc60
Daksh7112003 <13anil67@gmail.com>
2024-01-23 19:22:31 +0530

    DAY%

---
+++ DAY5.py
@@ -0,0 +1,49 @@
+
+#loops in pyhton
+
+a =1
+# while a<=10:
+#     print(a)
+#     a=a+1
+
+    #thats how you print the first 10 number in pyhton...
+
+
+
+while a<10:  
+        # print(a*3)
+        a=a+1
+# for printing the tables ...
+        
+
+
+i=1
+
+list=[12,2,3,4,5]
+
+while i <len(list):
+        list[i]=list[i]+100
+        i =i  +1
+
+
+print(list)      #bekar ke identification error ....
+
+
+
+
+
+
+
+
+#for loop
+
+b=0
+c=0
+hello1=['joota ', 'japani']
+hello2=['joota', "oppanda"]
+for b in hello1:
+        for  c in hello2:
+                
+                        print(b,c)
+                
+        
